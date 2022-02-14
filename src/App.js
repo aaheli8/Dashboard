10664d5c7aaee44d45b929c44a880576baa4002d
aaheli8 <sadhukhanaheli@gmail.com>
2022-02-14 11:33:53 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'bootstrap';
+
+function Sidebar1(){
+  return(
+    <div class="sidebar" style={{backgroundColor:"#696969"}}>
+            <ul>
+                <li class="nav-item mb-2"><h3>Brand Name</h3></li>
+                <li class="nav-item mb-2"><img src="logo.svg" style={{'borderRadius':'5px'}}></img></li>
+                <li class="nav-item mb-2">Hyat</li>
+                <li class="nav-item mb-2">Product Desiner</li>
+                <li class="nav-item mb-2"><button>Create Link</button></li>
+                <li class="nav-item mb-2">Overview</li>
+                <li class="nav-item mb-2">MyLinks</li>
+                <li class="nav-item mb-2">Extras</li>
+            </ul>
+       </div>
+  )
+}
+function Sidebar2()
+{
+  return(
+    <div  class="sidebar2">
+           <h2 class="dash">Overview</h2>
+            <div class="row">
+               <div class="column">Box1</div>
+               <div class="column">Box2</div>
+               <div class="column">Box3</div>
+            </div>
+       </div>
+  )
+}
+function Graph()
+{
+  return (
+    <div  class="sidebar2">
+            <div class="row">
+               <div class="column">Daily Insight</div>
+               <div class="column">
+                 <ul >
+                   <li class="list2">Api Version</li>
+                   <li class="list2">Frontend Version</li>
+                   <li class="list2">Links generated yet</li>
+                   <li class="list2">Total views yet</li>
+                 </ul>
+               </div>
+            </div>
+       </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    <Sidebar1 />
+    <Sidebar2 />
+    <Graph />
     </div>
   );
 }
